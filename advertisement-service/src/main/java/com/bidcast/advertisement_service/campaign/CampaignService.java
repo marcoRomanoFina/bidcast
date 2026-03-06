c2f7b3c557a4f9c32e6e3991580c2ad39c23f2ff
marco romano <marcoromanofinaa@gmail.com>
2026-03-06 15:20:38 +0000

    corrección de modelado sobre la creación de las campaigns y errores menores

--- advertisement-service/src/main/java/com/bidcast/advertisement_service/campaign/CampaignService.java
+++ advertisement-service/src/main/java/com/bidcast/advertisement_service/campaign/CampaignService.java
@@ -1,12 +1,15 @@
 package com.bidcast.advertisement_service.campaign;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bidcast.advertisement_service.campaign.dto.CampaignRequest;
 
+import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CampaignService {
@@ -14,14 +17,21 @@ public class CampaignService {
     private final CampaignRepository campaignRepository;
 
     @Transactional
-    public Campaign createCampaign(CampaignRequest data) {
+    public Campaign createCampaign(UUID advertiserId, CampaignRequest data) {
         
+        log.info("Creando nueva campaña en estado DRAFT: {}", data.name());
+
         Campaign campaign = Campaign.builder()
                 .name(data.name())
+                .advertiserId(advertiserId)
                 .budget(data.budget())
+                .bidCpm(data.bidCpm())
                 .status(CampaignStatusType.DRAFT) 
                 .build();
 
-        return campaignRepository.save(campaign);
+        Campaign saved = campaignRepository.save(campaign);
+        log.info("Borrador guardado exitosamente con ID: {}", saved.getId());
+
+        return saved;
     }
 }
